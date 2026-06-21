8e7a1b1daf5d433ccf8b99ab916ab612ff5234d8
vigneshmudgonda <124809262+vigneshmudgonda@users.noreply.github.com>
2026-06-21 18:02:10 +0530

    Update api.js

--- Frontend/src/axios/api.js
+++ Frontend/src/axios/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:4001/api",
+  baseURL: "https://book-store-ggdl.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
